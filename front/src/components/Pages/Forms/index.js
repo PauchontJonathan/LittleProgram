10c398c0317787d991194c1212641fb99785f750
PauchontJonathan <pauchontjonathan@gmail.com>
2020-05-10 18:38:51 +0200

    can register the current user and error messages work

--- front/src/components/Pages/Forms/index.js
+++ front/src/components/Pages/Forms/index.js
@@ -9,12 +9,21 @@ const Forms = () => {
   const [nicknameRegisterValue, setNicknameRegisterValue] = useState('');
   const [passwordRegisterValue, setPasswordRegisterValue] = useState('');
   const [passwordConfirmBoolean, setPasswordConfirmBoolean ] = useState(false);
+  const [errorMessageBoolean, setErrorMessageBoolean ] = useState(false);
+  const [successRegisterMessageBoolean, setSuccessRegisterMessageBoolean ] = useState(false);
+  const [successRegisterMessage, setSuccessRegisterMessage ] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [passwordVerify, setPasswordVerify ] = useState('');
 
   const handleWindow = () => {
     setOpenRegister(!openRegister);
     setEmptyValue(false);
     setPasswordConfirmBoolean(false);
+    setErrorMessageBoolean(false);
+    setSuccessRegisterMessageBoolean(false);
+    setNicknameRegisterValue('');
+    setPasswordRegisterValue('');
+    setPasswordVerify('');
   };
 
   const handleNicknameRegister = (evt) => {
@@ -32,6 +41,7 @@ const Forms = () => {
     setPasswordVerify(passwordConfirm);
   };
 
+  // Register form submit and verify
   const handleRegisterSubmit = (evt) => {
     evt.preventDefault();
     if (nicknameRegisterValue === '' || passwordRegisterValue === '' || passwordVerify === '' ) {
@@ -40,14 +50,27 @@ const Forms = () => {
       setPasswordConfirmBoolean(true);
     } else {
       setEmptyValue(false);
+      setEmptyValue(false);
       setPasswordConfirmBoolean(false);
       console.log(nicknameRegisterValue);
       console.log(passwordRegisterValue);
       axios.post('http://localhost:8000/api/v1/users/register', { nickname: nicknameRegisterValue, password: passwordRegisterValue })
         .then(res => {
-          console.log(res);
+          const { message } = res.data;
+          setPasswordConfirmBoolean(false);
+          setErrorMessageBoolean(false);
+          setSuccessRegisterMessageBoolean(true);
+          setSuccessRegisterMessage(message);
         }).catch(err => {
-          console.log(err.response.data);
+          const { message } = err.response.data.errorMessage.details[0];
+          if(message !== null ) {
+            setErrorMessageBoolean(true);
+            setErrorMessage(message);
+          } else if (err.response.data.errorMessage.details === undefined) {
+            const { error } = err.response.data;
+            setErrorMessageBoolean(true);
+            setErrorMessage(error);
+          }
         });
     }
   };
@@ -61,7 +84,9 @@ const Forms = () => {
           <a className="forms-form-register" onClick={handleWindow}>s'enregistrer</a>
           <h2 className="forms-form-title">connexion</h2>
           <input className="forms-form-input" type="text" name="nickname" placeholder="pseudo" />
+          <p className="forms-form-description">Le pseudo doit contenir au minimum 4 caractères</p>
           <input className="forms-form-input" type="password" name="password" placeholder="mot-de-passe" />
+          <p className="forms-form-description">Le mot-de-passe doit contenir au minimum 5 caractères</p>
           <input className="forms-form-input-password" type="submit" value="se connecter" />
         </form>
         )}
@@ -71,11 +96,15 @@ const Forms = () => {
             <a className="forms-form-register" onClick={handleWindow}>se connecter</a>
             <h2 className="forms-form-title">enregistrement</h2>
             <input className="forms-form-input" type="text" name="nickname" onChange={handleNicknameRegister} value={nicknameRegisterValue} placeholder="pseudo" />
+            <p className="forms-form-description">Le pseudo doit contenir au minimum 4 caractères</p>
             <input className="forms-form-input" type="password" name="password" onChange={handlePasswordRegister} value={passwordRegisterValue} placeholder="mot-de-passe" />
+            <p className="forms-form-description">Le mot-de-passe doit contenir au minimum 5 caractères</p>
             <input className="forms-form-input" type="password" name="passwordVerify" onChange={handlePasswordVerify} value={passwordVerify} placeholder="Confirmation de mot-de-passe" />
             <input className="forms-form-input-password" type="submit" value="s'enregistrer" />
             {passwordConfirmBoolean && <p className="forms-form-verification">Les mots de passes ne sont pas identiques !</p>}
             {emptyValues && <p className="forms-form-verification">Veuillez remplir tous les champs !</p>}
+            {errorMessageBoolean && <p className="forms-form-verification">{errorMessage}</p>}
+            {successRegisterMessageBoolean && <p className="forms-form-success">{successRegisterMessage}</p>}
           </form>
         )}
     </div>
